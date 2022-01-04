1bb412a82f723ff2be1a8a2ad4f3b18f45294490
eezj35 <justineezj0@gmail.com>
2022-01-04 14:35:04 +0800

    Add button and header components

--- cvwo-todo-app/src/App.js
+++ cvwo-todo-app/src/App.js
@@ -1,10 +1,10 @@
-
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo app</h1>
+      <Header title={'1'}/>
     </div>
   );
 }
---
+++ cvwo-todo-app/src/components/Header.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello'/>
+        </header>    
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string
+}
+
+export default Header
