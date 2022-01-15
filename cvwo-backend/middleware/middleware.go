05b22685b822f7913e0995e7e6f730546d6654c5
eezj35 <justineezj0@gmail.com>
2022-01-15 23:18:52 +0800

    Implement GetAllTask and CreateTask methods

--- cvwo-backend/middleware/middleware.go
+++ cvwo-backend/middleware/middleware.go
@@ -1,1 +1,87 @@
 package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/eezj35/CVWO_app/models"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading the .env file")
+	}
+}
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collectionName := os.Getenv("DB_COLLECTION_NAME")
+
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to mongodb.")
+
+	client.Database(dbName).Collection(collectionName)
+	fmt.Println("Collection instance created.")
+
+}
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	payload := getAllTasks()
+	json.NewEncoder(w).Encode(payload) // encode to send to frontend
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var task models.Todo
+	json.NewDecoder(r.Body).Decode(&task) // get data from json and decode
+	insertOneTask(task)
+	json.NewEncoder(w).Encode(task)
+}
+
+func CompleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UndoTask() {
+
+}
+
+func DeleteTask() {
+
+}
+
+func DeleteAllTasks() {
+
+}
